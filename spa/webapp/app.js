b252d5ae885134a7609acd2b35340889454c66ee
evgeniyfilippov <evgeniyfilippov@inbox.ru>
2016-07-02 22:27:31 +0400

    node server add express module

--- spa/webapp/app.js
+++ spa/webapp/app.js
@@ -1,8 +1,33 @@
-var http, server;
-http = require( 'http' );
-server = http.createServer( function ( request, response ) {
-    console.log(request);
-    response.writeHead( 200, { 'Content-Type': 'text/plain' } );
-    response.end( 'Hello World' );
-}).listen( 3000 );
-console.log( 'Прослушивается порт %d', server.address().port );
+'use strict';
+var http     = require( 'http' ),
+    express  = require( 'express' ),
+    app      = express(),
+    server   = http.createServer( app );
+
+//----------------SERVER CONFIG START----------------
+app.configure( function() {
+    app.use( express.bodyParser() );
+    app.use( express.methodOverride() );
+});
+
+app.configure( 'development', function() {
+    app.use( express.logger() );
+    app.use( express.errorHandler({
+        dumpExceptions : true,
+        showStack      : true
+    }) );
+});
+
+app.configure( 'production', function() {
+   app.use( express.errorHandler() ); 
+});
+
+app.get('/', function( request, response) {
+    response.send( 'Hello Express');
+});
+//----------------SERVER CONFIG END------------------
+server.listen(3000);
+console.log(
+    'Express-сервер прослушивает порт %d в режиме %s',
+    server.address().port, app.settings.env
+);
